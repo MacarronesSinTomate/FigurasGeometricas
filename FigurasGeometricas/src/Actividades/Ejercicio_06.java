29a3a7a769fa83f85a1e147e6cb4855ea83b3a0a
saber <saber@LAPTOP-020OOHDS>
2019-10-14 13:31:19 +0200

    Done!

--- FigurasGeometricas/src/Actividades/Ejercicio_06.java
+++ FigurasGeometricas/src/Actividades/Ejercicio_06.java
@@ -6,15 +6,26 @@ public class Ejercicio_06 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		int base = 0;
+		int espacios = 0;
 		
-		System.out.println("Introduce la base;: ");
+		System.out.println("(Debe ser un numero impar)");	
+		System.out.println("Introduce la base: ");
 		base = sc.nextInt();
+		espacios = base / 2;
 		
-		for (int i = 0; i < base; i++) {
-		
+		for (int i = 0; i < base; i+=2) {
+			
+			for (int o = 0; o < espacios; o++) {
+				System.out.print(" ");
+			}
+			for (int o = 0; o < i; o++) {
+				System.out.print("*");
+			}
+			espacios -= 1;
+			System.out.println("*");
 		}
 		
 		sc.close();
